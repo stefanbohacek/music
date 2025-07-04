acbe9ff1fd1dc7568cec76106371684e00f93fd2
Stefan Bohacek <stefan@stefanbohacek.com>
2025-07-03 22:14:46 -0400

    Track audio plays.

--- src/scripts/modules/matomo.js
+++ src/scripts/modules/matomo.js
@@ -1,8 +1,43 @@
-export default () => {
-  setTimeout(() => {
+export default async () => {
+  setTimeout(async () => {
     if (!window.Matomo) {
-      const url = `https://tools.stefanbohacek.com/info?idsite=26&action_name=${encodeURIComponent(document.title)}&url=${window.location.href}&uadata=${encodeURIComponent(JSON.stringify(navigator.userAgentData))}&res=${window.screen.availWidth}x${window.screen.availHeight}`;
-      fetch(url);
+      const url = `https://tools.stefanbohacek.com/info?idsite=26&action_name=${encodeURIComponent(
+        document.title
+      )}&url=${window.location.href}&uadata=${encodeURIComponent(
+        JSON.stringify(navigator.userAgentData)
+      )}&res=${window.screen.availWidth}x${window.screen.availHeight}`;
+      await fetch(url);
+
+      const audioElements = document.querySelectorAll("audio");
+
+      audioElements.forEach((audio) => {
+        audio.addEventListener("play", () => {
+          _paq.push([
+            "trackEvent",
+            "Audio",
+            "Play",
+            this?.dataset?.matomoTitle,
+          ]);
+        });
+
+        audio.addEventListener("pause", () => {
+          _paq.push([
+            "trackEvent",
+            "Audio",
+            "Pause",
+            this?.dataset?.matomoTitle,
+          ]);
+        });
+
+        audio.addEventListener("ended", () => {
+          _paq.push([
+            "trackEvent",
+            "Audio",
+            "Completed",
+            this?.dataset?.matomoTitle,
+          ]);
+        });
+      });
     }
   }, 2000);
 };
